d2197f7137b93eed0cb99620c938a296c45cb79a
Hugo Monteiro <monteiro.hugo@gmail.com>
2024-05-13 11:46:14 +0100

    Fix trailing new line on parameter pipe

--- sd2324-tp1/tls/gen_certificates.sh
+++ sd2324-tp1/tls/gen_certificates.sh
@@ -15,13 +15,13 @@ generateKeyStore() {
 	rm -f $filename.jks
 
 	echo "Generationg Public Key Pair: " $service " pass: " $pass 
-	
-	echo "$pass\n$pass\n$service\nTP2\nSD2324\nLX\nLX\nPT\nyes\n$pass\n$pass" | keytool -ext SAN=dns:$service -genkey -alias $service -keyalg RSA -validity 365 -keystore $filename.jks -storetype pkcs12
+
+	echo -n "$pass\n$pass\n$service\nTP2\nSD2324\nLX\nLX\nPT\nyes\n$pass\n$pass" | keytool -ext SAN=dns:$service -genkey -alias $service -keyalg RSA -validity 365 -keystore $filename.jks -storetype pkcs12
 
 	echo "Exporting certificate"
-	echo "$pass\n" | keytool -exportcert -ext SAN=dns:$service -alias $service -keystore $filename.jks -file $filename.cert
+	echo -n "$pass\n" | keytool -exportcert -ext SAN=dns:$service -alias $service -keystore $filename.jks -file $filename.cert
 	
-	echo "changeit\nyes\n" | keytool -importcert  -file $filename.cert -alias $service -keystore client-ts.jks
+	echo -n "changeit\nyes\n" | keytool -importcert  -file $filename.cert -alias $service -keystore client-ts.jks
 }
 
 generateKeyStore "users" "123users"
