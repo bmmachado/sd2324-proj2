29b444eb3602ab93bc2a7172451488bcdfec4d37
Hugo Monteiro <monteiro.hugo@gmail.com>
2024-05-13 15:38:23 +0100

    Preliminar TLS support for the REST servers

--- sd2324-tp1/src/tukano/impl/rest/servers/AbstractRestServer.java
+++ sd2324-tp1/src/tukano/impl/rest/servers/AbstractRestServer.java
@@ -6,13 +6,15 @@ import java.util.logging.Logger;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.net.ssl.SSLContext;
+
 import tukano.impl.discovery.Discovery;
 import tukano.impl.java.servers.AbstractServer;
 import utils.IP;
 
 
 public abstract class AbstractRestServer extends AbstractServer {
-	private static final String SERVER_BASE_URI = "http://%s:%s%s";
+	private static final String SERVER_BASE_URI = "https://%s:%s%s";
 	private static final String REST_CTX = "/rest";
 
 	protected AbstractRestServer(Logger log, String service, int port) {
@@ -24,12 +26,16 @@ public abstract class AbstractRestServer extends AbstractServer {
 		ResourceConfig config = new ResourceConfig();
 		
 		registerResources( config );
+
+    try {    
+  		JdkHttpServerFactory.createHttpServer( URI.create(serverURI.replace(IP.hostAddress(), INETADDR_ANY)), config, SSLContext.getDefault());
 		
-		JdkHttpServerFactory.createHttpServer( URI.create(serverURI.replace(IP.hostAddress(), INETADDR_ANY)), config);
+  		Discovery.getInstance().announce(service, super.serverURI);
 		
-		Discovery.getInstance().announce(service, super.serverURI);
-		
-		Log.info(String.format("%s Server ready @ %s\n",  service, serverURI));
+  		Log.info(String.format("%s Server ready @ %s\n",  service, serverURI));
+    } catch (Exception e) {
+      Log.info(String.format("%s Server NOT ready. Failed to start server\n"));
+    }
 	}
 	
 	abstract void registerResources( ResourceConfig config );
